98a36651bc247d8a4855734403eb4f583af6ceeb
Shankar <shankariyerr.s@gmail.com>
2019-09-02 03:33:33 +0530

    init

---
+++ code.js
@@ -0,0 +1,11 @@
+"use strict";
+figma.showUI(__html__, { width: 240, height: 200 });
+figma.ui.onmessage = msg => {
+    if (msg.type === 'minimise') {
+        figma.ui.resize(240,20);
+    }
+    if (msg.type === 'maximise') {
+        figma.ui.resize(240,200);
+    }
+    // figma.closePlugin();
+};
